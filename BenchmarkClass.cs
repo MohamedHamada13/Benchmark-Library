56885a8c9ec5c69e949511580a16a2d3cdc98160
mohamedHamdy <99550272+MohamedHamada13@users.noreply.github.com>
2024-02-14 14:40:42 -0800

    Add files via upload

---
+++ BenchmarkClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+
+namespace BenchmarkExample
+{
+    [MemoryDiagnoser(false)]
+    public class BenchmarkClass
+    {
+        // Method one
+        [Benchmark]
+        public int SumUsingChar()
+        {
+            int number = 123;
+            string strNumber = number.ToString();
+            int sum = 0;
+            foreach (char i in strNumber)
+            {
+                sum += (int)i - '0';
+            }
+            return sum;
+        }
+
+        // Method Two
+        [Benchmark]
+        public int SumUsingCalc()
+        {
+            int number = 123;
+            int sum = 0;
+            while (number > 0)
+            {
+                sum = (number % 10);
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+
+namespace BenchmarkExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<BenchmarkClass>();
+        }
+    }
+}
